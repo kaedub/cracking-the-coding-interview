7929d2fd6b3ae912d2c31016db0f4b5a812c066a
Kevin Welch <kaedub16@gmail.com>
2019-01-03 23:56:56 -0800

    Started URLify and checkPermutations problems

--- ch1-arrays-and-strings/exercises.cpp
+++ ch1-arrays-and-strings/exercises.cpp
@@ -7,12 +7,21 @@ using namespace std;
 /** Naive solution. What if you cannot use additional data structures? */
 bool isUnique(string s) {
   map<char, int> charCount;
+
   for (int i=0; i<s.length(); i++){
     charCount[s[i]]++;
     if (charCount[s[i]] > 1)
       return false;
   }
   return true;
+}
+
+bool checkPermutation(string s1, string s2) {
+  return s1 == s2;
+} 
+
+string URLify(string s) {
+  return s;
 }
 
 int main() {
